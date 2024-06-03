293249961048f4916c1fe9d2f765bc1cd5d1884c
rmadudtjr1 <rmadudtjr1@gmail.com>
2024-06-03 16:28:51 +0900

    df

--- notion/views.py
+++ notion/views.py
@@ -214,12 +214,11 @@ def sendEmail(request):
     url = request.GET.get('url');
     try :
         toEmail = request.GET.get('emails')
-        toEmail = toEmail.split(',')
+
         email = EmailMessage(
             f'{request.user.username} 공유', #이메일 제목
             f'''안녕하세요. {request.user.username} 님이 공유하신 페이지 입니다.
-
-http://localhost:8000/guest/{request.user.username}/{url}
+http://13.60.19.146/guest/{request.user.username}/{url}
             ''', #내용
             to=toEmail, #받는 이메일
         )
